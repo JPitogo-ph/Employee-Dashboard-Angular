ae8c5f28dbb94ed78005b61afb41b47bcdf612c9
james <jpitogodev@gmail.com>
2024-11-19 21:34:34 +0800

    Added employee table component and /employees route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { EmployeeTableComponent } from './employee-table/employee-table.component';
 
 export const routes: Routes = [
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent},
+    {path: 'employees', component: EmployeeTableComponent}
 ];
