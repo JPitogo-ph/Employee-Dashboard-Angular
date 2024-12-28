0d5f908f14916a9306689ea1537a20ac86fb2d44
james <jpitogodev@gmail.com>
2024-12-28 15:29:48 +0800

    Changed local to use https and service to use baseUrl instead of hardcoded string

--- src/app/employee-data.service.ts
+++ src/app/employee-data.service.ts
@@ -1,17 +1,16 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
 import { Employee } from './models/employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeDataService {
-  apiUrl = environment.baseUrl + "api/Employees"
+  apiUrl = environment.baseUrl + "/api/Employees"
   http = inject(HttpClient)
 
   getEmployees(): Observable<Employee[]> {
-     return this.http.get<Employee[]>('https://localhost:7044/api/employees')
+     return this.http.get<Employee[]>(this.apiUrl)
 }}
