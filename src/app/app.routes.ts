3ca6ff78414e5af7a68c68de47747e21e3a06b6f
james <jpitogodev@gmail.com>
2024-12-30 16:00:27 +0800

    Created new component to show detailed employee info, and updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EmployeeTableComponent } from './employee-table/employee-table.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
-    {path: 'employees', component: EmployeeTableComponent}
+    {path: 'employees', component: EmployeeTableComponent},
+    {path: 'employees/:id', component: EmployeeDetailsComponent}
 ];
