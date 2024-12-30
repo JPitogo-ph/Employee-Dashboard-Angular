80814e41d278c95c9955ffc19dac834770ea1b44
james <jpitogodev@gmail.com>
2024-12-30 20:46:25 +0800

    Refactored data service to use BehaviorSubject to cache the get request, also added function to get an Employee with a provided id. Note: This searches the cached data in the frontend, rather than making a request to the api.

--- src/app/employee-data.service.ts
+++ src/app/employee-data.service.ts
@@ -1,16 +1,44 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Employee } from './models/employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeDataService {
-  apiUrl = environment.baseUrl + "/api/Employees"
-  http = inject(HttpClient)
+  private apiUrl = environment.baseUrl + "/api/Employees";
 
-  getEmployees(): Observable<Employee[]> {
-     return this.http.get<Employee[]>(this.apiUrl)
-}}
+  private employeesCache =  new BehaviorSubject<Employee[] | null>(null);
+
+  private http = inject(HttpClient);
+
+  getEmployees(): Observable<Employee[] | null> {
+     if (this.employeesCache.value === null) {
+      this.http.get<Employee[]>(this.apiUrl).subscribe(
+        (data) => {
+          this.employeesCache.next(data);
+        }
+      )
+     }
+
+     return this.employeesCache.asObservable(); //Return as an immutable observable
+}
+
+  getEmployeeById(id: string): Observable<Employee> | null {
+    const cachedEmployees = this.employeesCache.value;
+    if (cachedEmployees) {
+      const employee = cachedEmployees.find((emp) => {
+        emp.employeeId === id;
+      })
+      if (employee) {
+        return new Observable<Employee>((observer) => {
+          observer.next(employee);
+          observer.complete()
+        })
+      }
+    }
+    return null //Is this a good idea in this language?
+  }
+}
