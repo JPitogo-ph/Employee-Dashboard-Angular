dd3a91eb59d85f9fcb3cda49ab33ce3d06ceef5f
james <jpitogodev@gmail.com>
2025-02-10 18:26:12 +0800

    Finished the form template, needs POST and error handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { EmployeeDetailsComponent } from './employee-details/employee-details.co
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { validateIdGuard } from './employee-details/validate-id.guard';
 import { employeeTableResolver } from './employee-table/employee-table.resolver';
+import { CreateNewEmployeeComponent } from './create-new-employee/create-new-employee.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'employees', component: EmployeeTableComponent, resolve: {data: employeeTableResolver}},
+    {path: 'employees/new', component: CreateNewEmployeeComponent},
     {path: 'employees/:id', component: EmployeeDetailsComponent, canActivate: [validateIdGuard]},
     {path:'**', component:PageNotFoundComponent}
 ];
--- src/app/create-new-employee/create-new-employee.component.ts
+++ src/app/create-new-employee/create-new-employee.component.ts
@@ -13,16 +13,26 @@ export class CreateNewEmployeeComponent {
 
   //This project is mainly a frontend learning experience. Take the easy way out and make sure backend always get's the right data (mostly).
   newEmployeeForm = this.fb.group({
-    employeeId: [0, Validators.required],
-    firstName: ['', Validators.required, Validators.minLength(2), Validators.maxLength(32)],
-    lastName: ['', Validators.required, Validators.minLength(2), Validators.maxLength(16)],
-    email: ['', Validators.required, Validators.email, Validators.min(2), Validators.maxLength(32)],
-    phoneNumber: ['', Validators.required, Validators.minLength(4), Validators.maxLength(16)],
-    hireDate: [Date.now()],
-    jobId: ['', Validators.required, Validators.minLength(4), Validators.maxLength(5)],
-    salary: [0, Validators.required],
-    commissionPct: [0.0],
-    managerId: [null],
+    employeeId: ['', Validators.required],
+    firstName: ['', {
+      validators: [Validators.required, Validators.minLength(2), Validators.maxLength(32)]
+    }],
+    lastName: ['', {
+      validators: [Validators.required, Validators.minLength(2), Validators.maxLength(16)]
+    }],
+    email: ['', {
+      validators: [Validators.required, Validators.email, Validators.min(2), Validators.maxLength(32)]
+   }],
+    phoneNumber: ['', {
+      validators: [Validators.minLength(4), Validators.maxLength(16)]
+    }],
+    hireDate: [Date.now(), Validators.required],
+    jobId: ['', {
+      validators: [Validators.required, Validators.minLength(4), Validators.maxLength(5)]
+    }],
+    salary: ['', Validators.required],
+    commissionPct: [''],
+    managerId: [''],
     departmentId: ['', Validators.required],
   })
 }
