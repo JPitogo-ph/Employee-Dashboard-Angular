982dd62b4feb87dc80f9684c2a93c6f97257de6c
james <jpitogodev@gmail.com>
2025-02-15 21:01:03 +0800

    Added methods for navigation across different components on the row as the (click) event on the <tr> was blocking navigation on the buttons.

--- src/app/employee-table/employee-table.component.ts
+++ src/app/employee-table/employee-table.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { EmployeeDataService } from '../employee-data.service';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-employee-table',
@@ -10,6 +10,21 @@ import { RouterLink } from '@angular/router';
   styleUrl: './employee-table.component.scss'
 })
 export class EmployeeTableComponent {
+  router = inject(Router);
+
   employeeService = inject(EmployeeDataService);
-  employeeList = this.employeeService.employeeCacheAccess
+  employeeList = this.employeeService.employeeCacheAccess;
+
+  onRowClick(employeeId: string) {
+    this.router.navigate(['/employees', employeeId]);
+  }
+
+  onEditClick(event: MouseEvent, employeeId: string) {
+    event.stopPropagation(); //There has to be a cleaner way
+    this.router.navigate(['/employees/edit', employeeId])
+  }
+
+  onDeleteClick(event: MouseEvent, employeeId: string) {
+    return true
+  }
 }
