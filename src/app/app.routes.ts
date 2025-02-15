2dd7ab7f7b64848a283bafcf1f191eb8181fb79e
james <jpitogodev@gmail.com>
2025-02-15 16:38:33 +0800

    Added new route for editing EditEmployee component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { validateIdGuard } from './employee-details/validate-id.guard';
 import { employeeTableResolver } from './employee-table/employee-table.resolver';
 import { CreateNewEmployeeComponent } from './create-new-employee/create-new-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -13,5 +14,6 @@ export const routes: Routes = [
     {path: 'employees', component: EmployeeTableComponent, resolve: {data: employeeTableResolver}},
     {path: 'employees/new', component: CreateNewEmployeeComponent},
     {path: 'employees/:id', component: EmployeeDetailsComponent, canActivate: [validateIdGuard]},
+    {path: 'employees/edit/:id', component: EditEmployeeComponent, canActivate: [validateIdGuard]},
     {path:'**', component:PageNotFoundComponent}
 ];
