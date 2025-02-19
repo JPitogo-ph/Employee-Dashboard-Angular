42ac2747f9360ebcea6e989c12dde5992a488412
james <jpitogodev@gmail.com>
2025-02-19 16:43:17 +0800

    Added logic to handle the new modal component for deletes.

--- src/app/employee-table/employee-table.component.ts
+++ src/app/employee-table/employee-table.component.ts
@@ -1,11 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { EmployeeDataService } from '../employee-data.service';
 import { Router, RouterLink } from '@angular/router';
+import { ModalComponent } from "./modal.component";
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'app-employee-table',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ModalComponent],
   templateUrl: './employee-table.component.html',
   styleUrl: './employee-table.component.scss'
 })
@@ -14,6 +16,9 @@ export class EmployeeTableComponent {
 
   employeeService = inject(EmployeeDataService);
   employeeList = this.employeeService.employeeCacheAccess;
+
+  isModalOpen = signal(false);
+  selectedEmployee = signal<Employee | null>(null)
 
   onRowClick(employeeId: string) {
     this.router.navigate(['/employees', employeeId]);
@@ -24,7 +29,13 @@ export class EmployeeTableComponent {
     this.router.navigate(['/employees/edit', employeeId])
   }
 
-  onDeleteClick(event: MouseEvent, employeeId: string) {
-    return true
+  onDeleteClick(event: MouseEvent, employee: Employee) {
+    event.stopPropagation();
+    this.selectedEmployee.set(employee)
+    this.isModalOpen.set(true);
+  }
+
+  closeModal(flag: boolean) {
+    this.isModalOpen.set(flag)
   }
 }
